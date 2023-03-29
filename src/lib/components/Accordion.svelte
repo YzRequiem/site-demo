75af3b6da963240658b1ff70e870f9fc548ad022
leo <mxmleosallier@gmail.com>
2023-03-29 14:15:12 +0200

    borderfix

--- src/lib/components/Accordion.svelte
+++ src/lib/components/Accordion.svelte
@@ -5,7 +5,7 @@
 
 	export let imagesTabAccordion = [];
 
-	export let sectionsTabAccordion = []
+	export let sectionsTabAccordion = [];
 
 	let selectedImage = 0;
 
@@ -13,33 +13,40 @@
 		return (selectedImage = index);
 	}
 
+	let hoverBorderNone = 'group-first:rounded-t-none';
+
 </script>
 
-<style>
-
-.min-w-200 {
-	min-width: 200px;
-}
-
-</style>
-
-<section class="w-full h-screen flex  items-center justify-center py-5">
+<section class="w-full h-screen flex items-center justify-center py-5">
 	<div class="flex flex-col py-5 w-full lg:w-8/12 center">
 		<Heading tag="h1" class="mb-4" customSize="text-3xl font-extrabold  md:text-5xl lg:text-6xl"
 			><Span gradient>Les Statistiques</Span>
 		</Heading>
 		<div class="flex py-5 w-full center flex-wrap">
-			<div class="w-full  min-w-200 xl:w-1/4 ">
+			<div class="w-full min-w-200 xl:w-1/4">
 				<Accordion class="mr-5 min-w-200 ">
 					{#each sectionsTabAccordion as section, index}
+						{#if index === 0}
+							<div on:click={() => updateSelectedImage(index)} on:keydown>
+								<AccordionItem>
+									<span slot="header">{section.title}</span>
+									<p class="mb-2 text-gray-500 dark:text-gray-400">
+										{section.content}
+									</p>
+								</AccordionItem>
+							</div>
+						{:else}									
+						
 						<div on:click={() => updateSelectedImage(index)} on:keydown>
-							<AccordionItem class="  ">
+							<AccordionItem class=" {hoverBorderNone} ">
 								<span slot="header">{section.title}</span>
-								<p class="mb-2 text-gray-500 dark:text-gray-400">
-									{section.content}
-								</p>
-							</AccordionItem>
-						</div>
+								
+									<p class="mb-2 text-gray-500 dark:text-gray-400">
+										{section.content}
+									</p>
+								</AccordionItem>
+							</div>
+						{/if}
 					{/each}
 				</Accordion>
 			</div>
@@ -49,3 +56,9 @@
 		</div>
 	</div>
 </section>
+
+<style>
+	.min-w-200 {
+		min-width: 200px;
+	}
+</style>
