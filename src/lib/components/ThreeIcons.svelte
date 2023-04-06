3fc3efb4466f19911628e9c36bcaff3bbae55044
leo <mxmleosallier@gmail.com>
2023-04-06 09:45:35 +0200

    refacto de threeIcons

--- src/lib/components/ThreeIcons.svelte
+++ src/lib/components/ThreeIcons.svelte
@@ -1,79 +1,56 @@
+<script>
+	export let header = {
+		title: 'Gérez votre commerce en toute simplicité',
+		content:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione dolores laborum labore provident impedit esse recusandae facere libero harum sequi.'
+	};
+
+	export let iconsTab = [
+		{
+			title: 'Gestion de stock efficace',
+			src: '/svg/box.svg',
+			alt: 'Stock'
+		},
+		{
+			title: 'Gestion de stock efficace',
+			src: '/svg/stats.svg',
+			alt: 'Stock'
+		},
+		{
+			title: 'Gestion de stock efficace',
+			src: '/svg/fuse.svg',
+			alt: 'rocket'
+		}
+	];
+</script>
+
 <section class="py-10 bg-white dark:text-white dark:bg-slate-900">
 	<div class="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 md:py-16 lg:px-8">
 		<div class="mx-auto max-w-3xl text-center">
 			<h2 class="text-3xl font-bold text-gray-900 dark:text-gray-200 sm:text-4xl">
-				Gérez votre commerce en toute simplicité
+				{header.title}
 			</h2>
 
 			<p class="mt-4 text-gray-500 sm:text-xl">
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione dolores laborum labore
-				provident impedit esse recusandae facere libero harum sequi.
+				{header.content}
 			</p>
 		</div>
 
 		<div class="mt-8 sm:mt-12">
 			<dl class="grid grid-cols-1 gap-4 sm:grid-cols-3">
-				<div class="flex flex-col rounded-lg border border-gray-500 px-4 py-8 text-center">
-					<dt class="order-last text-lg font-medium text-gray-500 dark:text-gray-400 py-5">
-						Gestion de stock efficace
-					</dt>
+				{#each iconsTab as box}
+					<div
+						class="flex flex-col rounded-lg border-gray-500 px-4 py-8 text-center border p-8 shadow-xl transition hover:border-[#23B7E5] hover:shadow-[#23B7E5] cursor-pointer"
+					>
+						<dt class="order-last text-lg font-medium text-gray-500 dark:text-gray-400 py-5">
+							{box.title}
+						</dt>
 
-					<dd class="  fill-[#23B7E5] flex justify-center">
-						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="w-14 h-14">
-							<path
-								d="M50.7 58.5L0 160H208V32H93.7C75.5 32 58.9 42.3 50.7 58.5zM240 160H448L397.3 58.5C389.1 42.3 372.5 32 354.3 32H240V160zm208 32H0V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V192z"
-							/></svg
-						>
-					</dd>
-				</div>
-
-				<div class="flex flex-col rounded-lg border border-gray-500 px-4 py-8 text-center ">
-					<dt class="order-last text-lg font-medium text-gray-500  dark:text-gray-400 py-5">
-						Statistiques de ventes en temps réel
-					</dt>
-
-					<dd class="text-4xl font-extrabold text-[#2594E4] md:text-5xl flex justify-center">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 24 24"
-							fill="currentColor"
-							class="w-14 h-14"
-						>
-							<path
-								fill-rule="evenodd"
-								d="M2.25 13.5a8.25 8.25 0 018.25-8.25.75.75 0 01.75.75v6.75H18a.75.75 0 01.75.75 8.25 8.25 0 01-16.5 0z"
-								clip-rule="evenodd"
-							/>
-							<path
-								fill-rule="evenodd"
-								d="M12.75 3a.75.75 0 01.75-.75 8.25 8.25 0 018.25 8.25.75.75 0 01-.75.75h-7.5a.75.75 0 01-.75-.75V3z"
-								clip-rule="evenodd"
-							/>
-						</svg>
-					</dd>
-				</div>
-
-				<div class="flex flex-col rounded-lg border border-gray-500 px-4 py-8 text-center">
-					<dt class="order-last text-lg font-medium text-gray-500 dark:text-gray-400 py-5">Optimisateur de vente</dt>
-
-					<dd class="text-4xl font-extrabold text-[#2594E4] md:text-5xl flex justify-center">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 24 24"
-							fill="currentColor"
-							class="w-14 h-14"
-						>
-							<path
-								fill-rule="evenodd"
-								d="M9.315 7.584C12.195 3.883 16.695 1.5 21.75 1.5a.75.75 0 01.75.75c0 5.056-2.383 9.555-6.084 12.436A6.75 6.75 0 019.75 22.5a.75.75 0 01-.75-.75v-4.131A15.838 15.838 0 016.382 15H2.25a.75.75 0 01-.75-.75 6.75 6.75 0 017.815-6.666zM15 6.75a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5z"
-								clip-rule="evenodd"
-							/>
-							<path
-								d="M5.26 17.242a.75.75 0 10-.897-1.203 5.243 5.243 0 00-2.05 5.022.75.75 0 00.625.627 5.243 5.243 0 005.022-2.051.75.75 0 10-1.202-.897 3.744 3.744 0 01-3.008 1.51c0-1.23.592-2.323 1.51-3.008z"
-							/>
-						</svg>
-					</dd>
-				</div>
+						<dd class="  flex justify-center">
+							<img src={box.src} alt={box.alt} class="w-14 h-14" />
+						</dd>
+					</div>
+				{/each}
 			</dl>
 		</div>
 	</div>
