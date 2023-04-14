adf8291080197113a97a73e90fc0f5886d97d7fe
leo <mxmleosallier@gmail.com>
2023-04-14 15:16:49 +0200

    ebauche histoire

--- src/routes/entreprise/+page.svelte
+++ src/routes/entreprise/+page.svelte
@@ -1,4 +1,124 @@
 <script>
-    /** @type {import('./$types').PageData} */
-    export let data;
+
 </script>
+
+<header class=" shadow">
+	<div class="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
+		<h1 class="text-3xl font-bold text-gray-900 py-5">Notre histoire - Silver-Stock</h1>
+	</div>
+</header>
+
+<section class="flex justify-center py-20">
+	<article class=" flex w-5/6 items-center justify-evenly flex-wrap ">
+		<div class="flex flex-col gap-20 w-3/6">
+            <div class="flex flex-col gap-5">
+                <h2 class="text-2xl ">Notre Mission</h2>
+			    <h3 class="text-3xl ">Redefinir la façon dont nous travaillons</h3> 
+            </div>
+			<div class=" flex flex-col gap-10">
+
+                <p>
+                    Dans un pays qui a vu naître les plus grandes références du luxe, croître les marques les
+                    plus raffinées, se développer les enseignes les plus créatives, comment imaginer que nous ne
+                    saurions accompagner l’innovation et la compétence au service de nos exigences ?
+                </p>
+                C’est pourquoi j’ai décidé, avec Silver Stock, d’aller au-devant des attentes des fumeurs et des
+                vapoteurs, en étant proactif : en proposant des vrais services, en vous proposant un accompagnement
+                personnalisé dans une relation de confiance sincère.
+                <p>
+                    J’ai voulu, à travers notre historie et nos investissements, à travers notre ancrage bleu
+                    blanc rouge, apporter à la cigarette électronique toutes ses lettres de noblesse.
+                </p>
+                <p>
+                    Je suis convaincu que tout cet ensemble, est autant de façons et de raisons de,
+                    naturellement, préférer Silver Stock. Silver-Stock a créé d'infinies possibilités que nous
+                    n'avions pas avant.
+			</p>
+
+        </div>
+			<p>Mossadegh AFRIT - Président Directeur Général</p>
+		</div>
+		<div>
+            <img class="max-w-[550px]" src="/pdg.jpg" alt="Mossadegh AFRIT - Président Directeur Général" />
+		</div>
+	</article>
+</section>
+
+<main class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+	<div class="px-4 py-8 sm:px-0">
+		<h2 class="text-2xl font-bold mb-4">Silver-Stock, qu'est-ce que c'est ?</h2>
+		<p class="mb-6">
+			Issue d'un travail mené depuis 2015, l'application Silver-Stock est aujourd'hui à son stade le
+			plus abouti et le plus fonctionnel. Développée par la synergie créée par des brainstorming
+			hebdomadaires entre les différents acteurs de deux entreprises de cigarette électronique
+			(équipes de ventes, préparateurs de commandes, responsables des achats, pôle managérial...) et
+			l'équipe de développement, ce projet s'est inscrit dans la modernisation de nos outils de
+			gestion, jusqu'à en devenir un élément central et essentiel, nous ayant permis de nous
+			démarquer des autres entreprises du secteur.
+		</p>
+		<p class="mb-6">
+			Toute la chaîne de vie d'un produit au sein de l'entreprise est gérée par l'application, de
+			son arrivée au dépôt à sa remise au client lors de la finalisation de la vente. Grâce à un
+			algorithme de prévision des besoins basé sur les statistiques de ventes des semaines
+			précédentes, les commandes sont optimisées au plus juste pour les semaines à venir. De fait,
+			tandis que la tendance est au surstock, ou à la rupture momentanée dans certaines enseignes,
+			nous pouvons nous targuer d'avoir à disposition l'outil calculant nos besoins au plus juste,
+			comprenant un léger stock tampon en prévision de hausses de ventes inattendues ou de
+			difficultés d'approvisionnement. Ainsi, nous évitons facilement les immobilisation financières
+			inutiles et les ruptures dans le même temps !
+		</p>
+		<p class="mb-6">
+			D'autre part, nous avons mis l'accent lors du développement de Silver-Stock sur une multitude
+			d'outils d’analyses statistiques. En temps réel ou sur une période donnée, nous pouvons avoir
+			à disposition visuelle l'ensemble des ventes réalisées au sein de nos boutiques, les volumes
+			de sortie de chacun des produits de notre catalogue, le nombre de nouveaux clients, de ventes
+			réalisées, le bénéfice produit, le chiffre d'affaire global ou par magasin,
+		</p>
+		<p class="mb-6">
+			et bien d'autres encore ! Nous pouvons ainsi, en un coup d'œil, avoir une vision globale de
+			l'activité de nos magasins, et nous permettre de prendre les bonnes décisions en temps réel.
+		</p>
+		<p class="mb-6">
+			Enfin, nous avons mis en place un système de gestion des stocks et des commandes qui nous
+			permet de suivre en temps réel l'ensemble des commandes en cours, et de gérer les retours
+			clients. Nous pouvons ainsi, en un coup d'œil, avoir une vision globale de l'activité de nos
+			magasins, et nous permettre de prendre les bonnes décisions en temps réel.
+		</p>
+
+		<h2 class="text-2xl font-bold mb-4">Nos valeurs</h2>
+		<p class="mb-6">
+			Silver-Stock est une application développée par des passionnés de cigarette électronique, qui
+			ont à cœur de vous proposer les meilleurs produits du marché, à des prix compétitifs. Nous
+			sommes une entreprise familiale, et nous avons à cœur de vous proposer des produits de
+			qualité, et de vous accompagner dans votre démarche de sevrage tabagique.
+		</p>
+		<p class="mb-6">
+			Nous sommes une entreprise familiale, et nous avons à cœur de vous proposer des produits de
+			qualité, et de vous accompagner dans votre démarche de sevrage tabagique.
+		</p>
+		<p class="mb-6">
+			Nous sommes une entreprise familiale, et nous avons à cœur de vous proposer des produits de
+			qualité, et de vous accompagner dans votre démarche de sevrage tabagique.
+		</p>
+
+		<h2 class="text-2xl font-bold mb-4">Notre équipe</h2>
+		<p class="mb-6">
+			Silver-Stock est une application développée par des passionnés de cigarette électronique, qui
+			ont à cœur de vous proposer les meilleurs produits du marché, à des prix compétitifs. Nous
+			sommes une entreprise familiale, et nous avons à cœur de vous proposer des produits de
+			qualité, et de vous accompagner dans votre démarche de sevrage tabagique.
+		</p>
+		<p class="mb-6">
+			Nous sommes une entreprise familiale, et nous avons à cœur de vous proposer des produits de
+			qualité, et de vous accompagner dans votre démarche de sevrage tabagique.
+		</p>
+
+		<h2 class="text-2xl font-bold mb-4">Nos partenaires</h2>
+		<p class="mb-6">
+			Silver-Stock est une application développée par des passionnés de cigarette électronique, qui
+			ont à cœur de vous proposer les meilleurs produits du marché, à des prix compétitifs. Nous
+			sommes une entreprise familiale, et nous avons à cœur de vous proposer des produits de
+			qualité, et de vous accompagner dans votre démarche de sevrage tabagique.
+		</p>
+	</div>
+</main>
