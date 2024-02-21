9db3e77d7c77565ba675a4a5c9615f9f995cef42
YzRequiem <mxmleosallier@gmail.com>
2024-02-21 16:28:59 +0100

    some new tutorials

---
+++ src/routes/assistance/utilisation/ventes/+page.svelte
@@ -0,0 +1,57 @@
+<script>
+	import ThreeIcons from '$lib/components/ThreeIcons.svelte';
+	import { page } from '$app/stores';
+
+	// store the current page name to display it in the header
+	let pageName = $page.url.pathname
+		.split('/')
+		.filter((segment) => segment !== '')
+		.pop();
+	pageName = pageName.charAt(0).toUpperCase() + pageName.slice(1);
+
+	let header3icons = {
+		title: pageName,
+		content: ' lorem ipsum lorem ipsum lorem ipsum lorem ipsum.'
+	};
+
+	let currentPath = $page.url.pathname;
+
+	let tabIcons = [
+		{
+			title: 'Réaliser une vente',
+			href: currentPath + '/creer'
+		},
+		{
+			title: 'Ajouter un client à une vente',
+			href: currentPath + '/ajouter-un-client'
+		},
+		{
+			title: 'Envoyer un reçu par email',
+			href: currentPath + '/email'
+		},
+		{
+			title: 'Imprimer un ticket de caisse',
+			href: currentPath + '/ticket-de-caisse'
+		},
+		{
+			title: 'Imprimer une facture',
+			href: currentPath + '/facture'
+		},
+		{
+			title: 'Echanger un produit',
+			href: currentPath + '/echanger-produit'
+		},
+		{
+			title: 'Les ventes en attente',
+			href: currentPath + '/vente-en-attente'
+		},
+		{
+			title: 'Ajouter des commentaires de vente',
+			href: currentPath + '/commentaires'
+		}
+	];
+</script>
+
+<main class="dark:bg-gray-900 dark:text-white shadow-lg dark:shadow-white">
+	<ThreeIcons header={header3icons} iconsTab={tabIcons} />
+</main>
