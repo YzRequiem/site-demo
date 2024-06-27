f6b588d9423358e905d830a1bab8eeb5ea28f1b1
YzRequiem <mxmleosallier@gmail.com>
2024-06-27 10:46:26 +0200

    Adding requests

--- src/routes/assistance/utilisation/requetes/+page.svelte
+++ src/routes/assistance/utilisation/requetes/+page.svelte
@@ -1,5 +1,15 @@
 <script>
-	import TutorielIncoming from '$lib/components/TutorielIncoming.svelte';
+	import Tutoriel from '$lib/components/Tutoriel.svelte';
+	export let pdf = {
+		link: '/pdf/Créer des requêtes produits.pdf',
+		fileName: 'Créer des requêtes produits.pdf'
+	};
 </script>
 
-<TutorielIncoming />
+<main class="dark:bg-gray-900 dark:text-white shadow-lg dark:shadow-white">
+	<Tutoriel
+		{pdf}
+		title="Créer des requêtes produits"
+		texte="Pour gérer la création de requêtes, suivez les étapes suivantes."
+	/>
+</main>
